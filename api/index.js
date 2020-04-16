4ebdd4e15b986e43855bc335ae782d80e0fcfe96
Samuel Newman <mozzius@protonmail.com>
2020-04-16 01:48:48 +0100

    trying hello world

--- api/index.js
+++ api/index.js
@@ -1,16 +1,5 @@
-const getPDF = require("../pdf");
+const getPDF = require('../pdf');
 
 module.exports = async (req, res) => {
-    try {
-        const { pathname = '/' } = parse(req.url, true);
-        let url = pathname.slice(1);
-        if (!url.startsWith('http')) {
-            url = 'https://' + url;
-        }
-        const pdf = await getPDF(url);
-        res.set('Content-type', 'application/pdf');
-        res.status(200).send(pdf);
-    } catch {
-        res.sendStatus(500);
-    }
+    res.status(200).send('Hello World')
 };
