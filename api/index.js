36a359835bddceb45db2e835a79a3eadeff48998
Samuel Newman <mozzius@protonmail.com>
2020-04-16 01:46:25 +0100

    expanded trycatch

--- api/index.js
+++ api/index.js
@@ -1,13 +1,12 @@
 const getPDF = require("../pdf");
 
 module.exports = async (req, res) => {
-    const { pathname = '/' } = parse(req.url, true);
-    let url = pathname.slice(1);
-    if (!url.startsWith('http')) {
-        url = 'https://' + url;
-    }
-    console.log(url);
     try {
+        const { pathname = '/' } = parse(req.url, true);
+        let url = pathname.slice(1);
+        if (!url.startsWith('http')) {
+            url = 'https://' + url;
+        }
         const pdf = await getPDF(url);
         res.set('Content-type', 'application/pdf');
         res.status(200).send(pdf);
